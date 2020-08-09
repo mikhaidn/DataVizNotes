ba3d0c704bbb786d9739e8f3cca2a60e90577101
Daniel Mikhail <daniel.mikhail@oath.com>
2020-08-08 22:25:43 -0500

    finish fuel efficiency

--- week8/axes.js
+++ week8/axes.js
@@ -6,6 +6,7 @@ i
 
 var xs = d3.scaleBand().domain([0,1,2,3,4,5]).range([0,200]);
 var ys = d3.scaleLinear().domain([0,42]).range([0,200]);
+var ysInv = d3.scaleLinear().domain([0,42]).range([200,0]);
 var margin = 50;
 var height = 200;
 
@@ -17,29 +18,12 @@ var rectangles = gee.selectAll("rect").data(data).enter().append("rect")
     .attr("width", xs.bandwidth())
     .attr("height",function(d){return ys(d)})
 
-rectangles.select("svg").append("g")
+d3.selectAll("svg").append("g")
     .attr("transform","translate("+margin+","+margin+")")
-    .call(d3.axisLeft(ys))
+    .call(d3.axisLeft(ysInv))
+
+d3.selectAll("svg").append("g")
+    .attr("transform","translate("+margin+","+(height+ margin)+")")
+    .call(d3.axisBottom(xs))
                 
 
-var rectangles = svgGroup.selectAll("rect").data(data).enter().append("rect")
-var rectangleAttr = rectangles.attr("x", function(d,i){return xs(i)})
-                 .attr("y", function(d){return ys(d)})
-                 .attr("width", xs.bandwidth())
-                 .attr("height",function(d){return 200 - ys(d)})
-
-
-                 var gee = d3.selectAll("svg").append("g")
-                 .attr("transform","translate("+margin+","+margin+")")
-                 .call(d3.axisLeft(ys)).append("g")
-                 .attr("transform",
-                       "translate("+margin+","+(height + margin)+")")
-                 .call(d3.axisBottom(xs));
-
-
-
-    path: class = "domain" stroke = "currentColor" d = "M-6,200.5H0.5V0.5H-6"
-g: class = "tick" opacity = "1" transform = "translate(0,200.5)"
-
-"M-6,200.5H0.5V0.5H-6"
-"M-6,0.5H0.5V200.5H-6"
--- week8/fuelEfficiency.js
+++ week8/fuelEfficiency.js
@@ -1,1 +1,32 @@
-// Fuel Efficiency
+
+data = await d3.csv("https://flunky.github.io/cars2017.csv")
+console.log(data)
+
+var xlogCityMPG = d3.scaleLog(10).domain([10,150]).range([0,200])
+var yrange = [0,200]
+var ylogHighwayMPG = d3.scaleLog(10).domain([10,150]).range(yrange)
+var margin = 50;
+var height = 200;
+var tickValues = [10,20,50,100]
+var tickFormat = d3.format("~s")
+
+var xs = xlogCityMPG;
+var ys = ylogHighwayMPG;
+var ysInv = d3.scaleLog(10).domain([10,150]).range([yrange[1],yrange[0]]);
+var margin = 50;
+var height = 200;
+
+var gee = d3.select("svg").append("g")
+.attr("transform","translate("+margin+","+margin+")")
+var rectangles = gee.selectAll("circle").data(data).enter().append("circle")
+    .attr("cx", function(d,i){return xs(d.AverageCityMPG)})
+    .attr("cy", function(d,i){return height - ys(parseInt(d.AverageHighwayMPG) )})
+    .attr("r", function(d,i){return (2 + parseInt(d.EngineCylinders) )})
+
+d3.selectAll("svg").append("g")
+    .attr("transform","translate("+margin+","+margin+")")
+    .call(d3.axisLeft(ysInv).tickValues(tickValues).tickFormat(tickFormat))
+
+d3.selectAll("svg").append("g")
+    .attr("transform","translate("+margin+","+(height+ margin)+")")
+    .call(d3.axisBottom(xs).tickValues(tickValues).tickFormat(tickFormat))
